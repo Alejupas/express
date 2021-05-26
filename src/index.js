3532dbdf1334dbde38933e2ab23cca7820912bc2
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-26 13:46:47 +0300

    static path , people file added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 //isitraukiam express is npm
 const express = require('express');
 const path = require('path');
+
+const { people } = require('./js/people');
 //skuriam express app objekta
 const app = express();
 
+// current paths
+const htmlPath = path.join(__dirname, 'html');
 const indexPath = path.join(__dirname, 'html', 'index.html');
 const aboutPath = path.join(__dirname, 'html', 'about.html');
-console.log('indexPath', indexPath);
+// console.log('indexPath', indexPath);
+
+// kai turim papke kurios failus norim pasiekti is narsykles pagal pavadinimus ,
+// nustatom static papke
+app.use(express.static(htmlPath));
 
 //main route
-app.get('/', (req, res) => {
-  //   res.send('<h1>Hello pacan backender from express</h1>');
-  res.sendFile(indexPath);
-});
-app.get('/about', (req, res) => res.sendFile(aboutPath));
+// app.get('/', (req, res) => {
+//   //   res.send('<h1>Hello pacan backender from express</h1>');
+//   res.sendFile(indexPath);
+// });
+// app.get('/about', (req, res) => res.sendFile(aboutPath));
 
 console.log('works');
 
