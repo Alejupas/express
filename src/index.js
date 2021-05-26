b47de5e18c8c84ca4315af2c87ec4e0b3f11d95f
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-26 12:09:00 +0300

    express working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-'use strict';
-console.log('cia yra express paskaita');
+//isitraukiam express is npm
+const express = require('express');
+
+//skuriam express app objekta
+const app = express();
+
+//main route
+app.get('/', (req, res) => {
+  res.send('<h1>Hello pacan backender from express</h1>');
+});
+
+app.listen(3000, () => console.log('server is running'));
