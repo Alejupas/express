16c317f8c43eb97a9915c73a07b7aed86aa58308
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-26 13:25:21 +0300

    added sending html, res.sendFile ir filelog

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 //isitraukiam express is npm
 const express = require('express');
-
+const path = require('path');
 //skuriam express app objekta
 const app = express();
 
+const indexPath = path.join(__dirname, 'html', 'index.html');
+console.log('indexPath', indexPath);
+
 //main route
 app.get('/', (req, res) => {
-  res.send('<h1>Hello pacan backender from express</h1>');
+  //   res.send('<h1>Hello pacan backender from express</h1>');
+  res.sendFile(indexPath);
 });
 
+console.log('works');
+
+//paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
 app.listen(3000, () => console.log('server is running'));
