0ed339fb1dd48d1710a53436a7f33e317cae9e5f
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-27 11:44:15 +0300

    upd

--- server/routes/api/peopleApi.js
+++ server/routes/api/peopleApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+let personId = 6;
 const people = require('../../js/peopleData');
 
 // get all people Endpoint
@@ -27,15 +27,36 @@ router.get('/:id', (req, res) => {
 // Create one people Endpoint
 // gauti duomenis is vartojo formos arba json pavidalu ir sukuri nauja partotoja tarp savo people
 router.post('/', (req, res) => {
-  console.log('this is what was set to server in body ', req.body);
-  // prideti nauja people objekta(zmogu pridedu)
+  console.log(' This is what was set to server in body', req.body); //{ name: 'James', surname: 'Jameson' }
+  // prideti nauja people objekta
+
   const newPerson = {
     id: (++personId).toString(),
     name: req.body.name,
     surname: req.body.surname,
   };
+
   people.push(newPerson);
   res.json(people);
+});
+
+// edit one peope Endpoint
+router.put('/:id', (req, res) => {
+  console.log('executing put request');
+  const paramId = req.params.id;
+
+  const found = people.find((p) => p.id === paramId);
+  if (!found) {
+    res
+      .status(404)
+      .json({ errorMsg: `sorry person with id ${paramId} was not found` });
+  }
+
+  const { name, surname } = req.body;
+  // atnjaujinti zmongu
+  found.name = name || found.name; //pipe (or) operatorius
+  found.surname = surname ? surname : found.surname;
+  res.json({ msg: 'User was updated', updatedUser: found });
 });
 
 module.exports = router;
